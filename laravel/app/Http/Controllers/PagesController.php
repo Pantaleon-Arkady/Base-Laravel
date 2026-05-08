1d8d3afcda697616090881c818e136104d0da938
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-05-08 16:09:54 +0800

    Homepage function controller with workouts data query

--- laravel/app/Http/Controllers/PagesController.php
+++ laravel/app/Http/Controllers/PagesController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Workout;
 
 class PagesController extends Controller
 {
+    public function home(Request $request)
+    {
+        $workouts = Workout::with('exercises')
+            ->where('user_id', $request->user()->id)
+            ->latest()
+            ->get();
+
+        return view('pages.home', [
+            'workouts' => $workouts
+        ]);
+    }
+
     public function login()
     { return view('pages.signup', ['mode' => 'login']); }
 
