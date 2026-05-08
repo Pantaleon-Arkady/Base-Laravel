f9155b8ea3024c477c7556e015b2116598e22f9f
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-05-08 11:00:31 +0800

    Add exercise   - workout form function   - init data   - add exerise field function
    close #22

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,2 +1,35 @@
 import 'bootstrap';
 import '../css/app.css';
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+window.workoutForm = function () {
+    return {
+        workout: '',
+        exercises: [
+            {
+                name: '',
+                type: 'bodyweight',
+                sets: '',
+                reps: '',
+                weight: '',
+                duration: ''
+            }
+        ],
+
+        addExercise() {
+            this.exercises.push({
+                name: '',
+                type: 'bodyweight',
+                sets: '',
+                reps: '',
+                weight: '',
+                duration: ''
+            })
+        }
+    }
+}
+
+
+Alpine.start()
