4b6548a293099fd7d4b8183e6e88fe0851594831
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-05-12 11:10:45 +0800

    signup orientation for both mobile and desktop

--- laravel/resources/views/pages/signup.blade.php
+++ laravel/resources/views/pages/signup.blade.php
@@ -6,42 +6,53 @@
 
     <div class="signup_main_div bg-secondary border border-2 border-black">
 
-        @if ($mode === 'login')
+        <div class="d-none d-md-flex signup_banner_div">
+            Left sign up
+        </div>
+        <div class="sign_up_content_div">
+            @if ($mode === 'login')
 
-            <form class="signup_forms_element d-flex flex-column" method="POST" action="/login">
-                <div class="signup_forms_header">Login</div>
-                @csrf
-                <input class="signup_inputs" type="text" name="namemail" placeholder="Username or Email..." />
-                <input class="signup_inputs" type="password" name="password" placeholder="Password..." />
+                <div class="signup_content_wrapper">
+                    <form class="signup_forms_element d-flex flex-column" method="POST" action="/login">
+                        <div class="signup_forms_header">Login</div>
+                        @csrf
+                        <input class="signup_inputs" type="text" name="namemail" placeholder="Username or Email..." />
+                        <input class="signup_inputs" type="password" name="password" placeholder="Password..." />
+                        
+                        <button class="btn btn-success mt-3">Login</button>
+                    </form>
                 
-                <button class="btn btn-success mt-3">Login</button>
-            </form>
-        
-            <div class="signup_toggle mt-4">
-                <span>Don't have an account yet?</span>
-                <a href="/signup/register" class="btn btn-success">
-                    Register
-                </a>
-            </div>
-        @else
-
-            <form class="signup_forms_element d-flex flex-column" method="POST" action="/register">
-                <div class="signup_forms_header">Register</div>
-                @csrf
-                <input class="signup_inputs" type="text" name="name" placeholder="Create username..." />
-                <input class="signup_inputs" type="email" name="email" placeholder="Enter email..." />
-                <input class="signup_inputs" type="password" name="password" placeholder="Create password..." />
+                    <div class="signup_toggle mt-4">
+                        <span>Don't have an account yet?</span>
+                        <a href="/signup/register" class="btn btn-success">
+                            Register
+                        </a>
+                    </div>
+                </div>
                 
-                <button class="btn btn-success mt-3">Register</button>
-            </form>
+            @else
 
-            <div class="signup_toggle mt-4">
-                <a href="/signup/login" class="btn btn-success">
-                    Log in ?
-                </a>
-            </div>
+                <div class="signup_content_wrapper">
+                    <form class="signup_forms_element d-flex flex-column" method="POST" action="/register">
+                        <div class="signup_forms_header">Register</div>
+                        @csrf
+                        <input class="signup_inputs" type="text" name="name" placeholder="Create username..." />
+                        <input class="signup_inputs" type="email" name="email" placeholder="Enter email..." />
+                        <input class="signup_inputs" type="password" name="password" placeholder="Create password..." />
+                        
+                        <button class="btn btn-success mt-3">Register</button>
+                    </form>
 
-        @endif
+                    <div class="signup_toggle mt-4">
+                        <span>Already have an account?</span>
+                        <a href="/signup/login" class="btn btn-success">
+                            Log in ?
+                        </a>
+                    </div>
+                </div>
+
+            @endif
+        </div>
 
     </div>
 
